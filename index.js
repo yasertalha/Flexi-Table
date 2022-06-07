03e235bba3582446a6599acfe7f3ea01b18d784a
Syed Yaser Mohasin <47635290+yasertalha@users.noreply.github.com>
2022-06-07 19:15:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./src/App";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
